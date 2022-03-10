80db12da78c1efea4317e247668708166439dc90
Kathryn Crowter <kcrowter@phas.ubc.ca>
2022-03-10 13:26:49 -0800

    minor bug

--- plot_mask.py
+++ plot_mask.py
@@ -1,5 +1,6 @@
 import numpy as np
 import argparse
+from matplotlib import pyplot as plt
 from presto_without_presto import rfifind
 
 def array_from_mask_params(nint, nchan, zap_ints, zap_chans, zap_chans_per_int):
