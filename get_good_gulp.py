0ff1969c57d52823fe86aab8307d5e1470f75b42
Kathryn Crowter <kcrowter@phas.ubc.ca>
2022-03-23 11:47:05 -0700

    get_good_gulp: thin info, top half of band option

--- get_good_gulp.py
+++ get_good_gulp.py
@@ -5,21 +5,22 @@ import time
 import argparse
 import sys
 
+
 t0 = time.perf_counter()
 
 #######################################################################
 ########################## DEFINE FUNCTIONS ###########################
 
 
-def sizeof_fmt(num, use_kibibyte=False):
+def sizeof_fmt(num, use_kibibyte=False, dp=1):
     """Make number of bytes human-readable, default is 1kB = 1000B
     To use 1024, set use_kibibyte=True"""
     base, suffix = [(1000.,'B'),(1024.,'iB')][use_kibibyte]
     for x in ['B'] + list(map(lambda x: x+suffix, list('kMGTP'))):
         if -base < num < base:
-            return "%3.1f %s" % (num, x)
+            return f"{num:3.{dp}f} {x}"
         num /= base
-    return "%3.1f %s" % (num, x)
+    return f"{num:3.{dp}f} {x}"
 
 
 def factorize(num):
@@ -93,6 +94,7 @@ def check_positive_float(value):
         raise argparse.ArgumentTypeError("%s is an invalid positive float value" % value)
     return fvalue
 
+
 ########################## DEFINE FUNCTIONS ###########################
 #######################################################################
 
@@ -121,9 +123,13 @@ parser.add_argument('-f', '--fudge', default=1, type=float,
 )
 parser.add_argument('-l', '--last_only', action='store_true', help="only print last line (useful with <max>)")
 
+parser.add_argument('--fdmt', action='store_true', help="Get good gulp size for fdmt instead")
+
+parser.add_argument('--thin', action='store_true', help="Thin the output gulps to only the max and min, for that number of chunks, under the max size limit")
+
 parser.add_argument('-v', '--verbosity', action='count', default=0)
 
-parser.add_argument('--fdmt', action='store_true', help="Get good gulp size for fdmt instead")
+parser.add_argument('--tophalf', action='store_true', help="only use with --fdmt. Run on only top half of the band")
 
 args = parser.parse_args()
 
@@ -161,6 +167,9 @@ nchans = header["nchans"]
 # calculate fmin and fmax FROM HEADER
 fmax = header["fch1"] - header["foff"]/2
 fmin = fmax + nchans * header["foff"]
+if args.fdmt and args.tophalf:
+    fmin = fmin + (fmax-fmin)/2
+    nchans = nchans // 2
 verbose_message(2, f"fmin: {fmin}, fmax: {fmax}, nchans: {nchans} tsamp: {tsamp}")
 
 
@@ -191,29 +200,48 @@ verbose_message(1, f"Maximum DM delay need to shift by is {maxdelay_s} s")
 verbose_message(1, f"This corresponds to {maxDT} time samples")
 print(f"Minimum gulp size is {maxDT}")  # I think mid_array being 0 size won't break anything? Don't know how the write handles that
 
+if args.fdmt:
+    fd1 = FDMT(fmin=fmin, fmax=fmax, nchan=nchans, maxDT=maxDT)
 
-factors = np.array(factorize(nsamples))
-factors_over_maxDT = factors[factors > maxDT]
-verbose_message(1, f"\ngulps > maxDT ({maxDT}) which are also factors of nsamples ({nsamples}): \n{factors_over_maxDT}")
+# OLD: based on factors of nsamples
+#factors = np.array(factorize(nsamples))
+#factors_over_maxDT = factors[factors > maxDT]
+#possible_gulps = factors_over_maxDT
+#verbose_message(1, f"\ngulps > maxDT ({maxDT}) which are also factors of nsamples ({nsamples}): \n{factors_over_maxDT}")
+
+# NEW: what actually want is for all gulps to be > maxDT
+print(f"Number of samples: {nsamples}")
+gulps_over_maxDT = np.arange(maxDT+1, nsamples) # haven't tested whether it throws a hissy fit if gulp=maxDT, so being safe
+leftovers = nsamples % gulps_over_maxDT
+no_leftovers = gulps_over_maxDT[leftovers == 0]
+no_data_lost = gulps_over_maxDT[leftovers > maxDT]
+possible_gulps = list(no_data_lost)
+possible_gulps.extend(list(no_leftovers))
+possible_gulps.sort()
+left = [nsamples % g for g in possible_gulps]
 
 
 verbose_message(1, "Memory size below is an approximation and based on the size of the main arrays for the calculation")
-verbose_message(1, "(Based on very minimal testing on a small observation!) it can be 2x this")
+#verbose_message(1, "(Based on very minimal testing on a small observation!) it can be 2x this")
 
 print("")
 print(f"{'gulp':<20} {'nchunks':<10} {'approx size':<14} {'x1.25':<10} {'x1.5':<10} {'x2':<10}")
 nbytes = header['nbits'] // 8
 if nbytes < 4:
-    verbose_message(0, f"WARNING {nbytes} bytes used for size estimation BUT if using a mask everyhing gets converted to floats - times by ~{int(4/nbytes)}")
-prev = []
-for gulp in factors_over_maxDT:
+    verbose_message(0, f"WARNING {nbytes} bytes used for size estimation BUT if using a mask everyhing gets converted to floats - times by {int(4/nbytes)}")
+#prev = []
+
+dt = [('gulp', int), ('nchunks', int), ('leftover', int), ('byte_size_data', float)]
+data = []
+for i in range(len(possible_gulps)):
+    gulp = possible_gulps[i]
+    leftover = left[i]
     nchunks = int(nsamples / gulp)
 
     if args.fdmt:
         byte_size_intensities = gulp*nchans*nbytes
         byte_size_prev_arr = maxDT*maxDT*nbytes
 
-        fd1 = FDMT(fmin=fmin, fmax=fmax, nchan=nchans, maxDT=maxDT)
         numRowsA = (fd1.subDT(fd1.fs)).sum()
         numRowsB = (fd1.subDT(fd1.fs[::2], fd1.fs[2] - fd1.fs[0])).sum()
         byte_size_A = (gulp + 2*maxDT)*numRowsA*nbytes
@@ -224,21 +252,33 @@ for gulp in factors_over_maxDT:
     else:
         byte_size_data = (2*gulp*nchans + maxDT*nchans)*nbytes
 
+    data.append((gulp, nchunks, leftover, byte_size_data))
+gulps_and_info = np.array(data, dtype=dt)
 
+within_size_lim = gulps_and_info[gulps_and_info['byte_size_data'] * args.fudge <= args.max]
 
-    if args.fudge * byte_size_data > args.max:
-        if args.last_only:
-            gulp, nchunks, byte_size_data = prev
-            print(f"{gulp:<20} {nchunks:<10} {sizeof_fmt(byte_size_data):<14} "
-                  f"{sizeof_fmt(1.25*byte_size_data):<10} "
-                  f"{sizeof_fmt(1.5*byte_size_data):<10} "
-                  f"{sizeof_fmt(2*byte_size_data):<10}")
-        break
+prec=2
 
-    if not args.last_only:
-        print(f"{gulp:<20} {nchunks:<10} {sizeof_fmt(byte_size_data):<14} "
-              f"{sizeof_fmt(1.25*byte_size_data):<10} "
-              f"{sizeof_fmt(1.5*byte_size_data):<10} "
-              f"{sizeof_fmt(2*byte_size_data):<10}")
-
-    prev = [gulp, nchunks, byte_size_data]
+if args.last_only:
+    gulp, nchunks, leftover, byte_size_data = within_size_lim[-1]
+    print(f"{gulp:<20} {nchunks:<10} {sizeof_fmt(byte_size_data, dp=prec):<14} "
+          f"{sizeof_fmt(1.25*byte_size_data, dp=prec):<10} "
+          f"{sizeof_fmt(1.5*byte_size_data, dp=prec):<10} "
+          f"{sizeof_fmt(2*byte_size_data, dp=prec):<10}")
+elif args.thin:
+    for nchunk in np.unique(within_size_lim['nchunks'])[::-1]:
+        same_nchunk = within_size_lim[within_size_lim['nchunks'] == nchunk]
+        mingulp_idx = np.where(same_nchunk['gulp'] == same_nchunk['gulp'].min())[0]
+        maxgulp_idx = np.where(same_nchunk['gulp'] == same_nchunk['gulp'].max())[0]
+        for id in [mingulp_idx, maxgulp_idx]:
+            gulp, nchunks, leftover, byte_size_data = same_nchunk[id][0]
+            print(f"{gulp:<20} {nchunks:<10} {sizeof_fmt(byte_size_data, dp=prec):<14} "
+                  f"{sizeof_fmt(1.25*byte_size_data, dp=prec):<10} "
+                  f"{sizeof_fmt(1.5*byte_size_data, dp=prec):<10} "
+                  f"{sizeof_fmt(2*byte_size_data, dp=prec):<10}")
+else:
+    for (gulp, nchunks, leftover, byte_size_data) in within_size_lim:
+        print(f"{gulp:<20} {nchunks:<10} {sizeof_fmt(byte_size_data, dp=prec):<14} "
+              f"{sizeof_fmt(1.25*byte_size_data, dp=prec):<10} "
+              f"{sizeof_fmt(1.5*byte_size_data, dp=prec):<10} "
+              f"{sizeof_fmt(2*byte_size_data, dp=prec):<10}")
