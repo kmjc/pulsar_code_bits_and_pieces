b066b55bed1cd5961dc7c628840a53811c3796bd
Kathryn Crowter <kcrowter@phas.ubc.ca>
2022-03-24 17:51:07 -0700

    ggg: index error from prev commit

--- get_good_gulp.py
+++ get_good_gulp.py
@@ -343,13 +343,6 @@ elif args.thin:
     for nchunk in np.unique(within_size_lim["nchunks"])[::-1]:
         same_nchunk = within_size_lim[within_size_lim["nchunks"] == nchunk]
         if same_nchunk.shape[0] == 1:
-            ids = [0]
-        else:
-            ids = [
-                np.where(same_nchunk["gulp"] == same_nchunk["gulp"].min())[0],
-                np.where(same_nchunk["gulp"] == same_nchunk["gulp"].max())[0]
-            ]
-        for id in ids:
             gulp, nchunks, leftover, byte_size_data = same_nchunk[id][0]
             print(
                 f"{gulp:<20} {nchunks:<10} {sizeof_fmt(byte_size_data, dp=prec):<14} "
@@ -357,6 +350,19 @@ elif args.thin:
                 f"{sizeof_fmt(1.5*byte_size_data, dp=prec):<10} "
                 f"{sizeof_fmt(2*byte_size_data, dp=prec):<10}"
             )
+        else:
+            ids = [
+                np.where(same_nchunk["gulp"] == same_nchunk["gulp"].min())[0],
+                np.where(same_nchunk["gulp"] == same_nchunk["gulp"].max())[0]
+            ]
+            for id in ids:
+                gulp, nchunks, leftover, byte_size_data = same_nchunk[id][0]
+                print(
+                    f"{gulp:<20} {nchunks:<10} {sizeof_fmt(byte_size_data, dp=prec):<14} "
+                    f"{sizeof_fmt(1.25*byte_size_data, dp=prec):<10} "
+                    f"{sizeof_fmt(1.5*byte_size_data, dp=prec):<10} "
+                    f"{sizeof_fmt(2*byte_size_data, dp=prec):<10}"
+                )
 else:
     for (gulp, nchunks, leftover, byte_size_data) in within_size_lim:
         print(
