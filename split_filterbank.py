0e72cf025746c82f47b8e4e8437dc22beb5ff441
Kathryn Crowter <kcrowter@phas.ubc.ca>
2024-02-28 16:31:28 -0800

    split_filterbank: edit to run within smaller memory

--- split_filterbank.py
+++ split_filterbank.py
@@ -20,7 +20,7 @@ def split_fil(fname, nfiles):
     filfile = open(fname, "rb")
     filfile.seek(hdrlen)
 
-    for i, gulp in enumerate(gulps):
+    for i, gulp in enumerate(gulps, subgulp=100000):
         out_fname = f"{fname.rstrip('.fil')}_{i}.fil"
         header_out = copy.deepcopy(header)
         header_out['tstart'] = header['tstart'] + sum(gulps[:i+1]) * tsamp / 60 / 60 / 24
@@ -28,8 +28,13 @@ def split_fil(fname, nfiles):
         outf = open(out_fname, "wb")
         write_header(header_out, outf)
 
-        intensities = np.fromfile(filfile, count=gulp*nchans, dtype=arr_dtype)
-        outf.write(intensities.ravel().astype(arr_dtype))
+        subgulps = [subgulp]*int(gulp//subgulp)
+        if gulp % subgulp:
+            subgulps.append(int(gulp % subgulp))
+
+        for subgulpp in subgulps:
+            intensities = np.fromfile(filfile, count=subgulpp*nchans, dtype=arr_dtype)
+            outf.write(intensities.ravel().astype(arr_dtype))
 
         outf.close()
         print(f"Wrote {out_fname}")
@@ -55,8 +60,14 @@ if __name__ == "__main__":
         help="Split filterbank into <nfiles> files",
     )
 
+    parser.add_argument(
+        "subgulp",
+        defualt=100000,
+        help="how many time samples to read at once (100000 with 1024 channels and uint8 => ~100 MB)"
+    )
+
     args = parser.parse_args()
 
-    split_fil(args.fname, args.nfiles)
+    split_fil(args.fname, args.nfiles, subgulp=args.subgulp)
     sys.exit(0)
 
