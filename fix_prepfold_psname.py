54b3f40b763d79eeec8a4372896273fd27c79267
Kathryn Crowter <kcrowter@phas.ubc.ca>
2024-05-16 15:29:53 -0700

    fix_prepfold_psname.py: fix

--- fix_prepfold_psname.py
+++ fix_prepfold_psname.py
@@ -1,31 +1,41 @@
 import argparse
 import glob
 import os
+from copy import deepcopy
 
 def rename_ps(dirname, verbose=True):
-    print(f"# Renaming {dirname}/*.ps files if they were too long")
+    if verbose:
+        print(f"# Renaming {dirname}/*.ps files if they were too long")
     pfds = sorted(glob.glob(os.path.join(dirname, "*.pfd")))
     allfnames = sorted(glob.glob(os.path.join(dirname, "*")))
+    pss_existing = sorted(glob.glob(os.path.join(dirname, "*.ps")))
+    pss_expect = [f"{pfd[:-3]}.ps" for pfd in pfds]
+    weird_files = [f for f in allfnames if (f[-4:] != ".pfd" and f[-9:] != ".bestprof" and f[-3:] != ".ps")]
 
-    for i, pfd in enumerate(pfds):
-        basename = os.path.basename(pfd)
-        if len(f"{basename}.ps") > 73:
-            new_name = f"{pfd}.ps"
-            old_name = os.path.join(os.path.dirname(new_name), os.path.basename(new_name)[:73])
-            # check it exists
-            if new_name in allfnames:
-                if verbose:
-                    print(f"{new_name} already exists")
-                continue
-            if old_name not in allfnames:
-                raise RuntimeWarning(f"Expect {old_name} to exist, but it doesn't")
-            if verbose:
-                print(f"Moving {old_name} to {new_name}")
-            os.rename(old_name, new_name)
+    rename = {}
+    for i, pfd in enumerate(deepcopy(pfds)):
+        if pss_expect[i] in pss_existing:
+            del pfds[i]
+        found = False
+        for j, weird in enumerate(deepcopy(weird_files)):
+            if weird in pss_expect[i]:
+                if found:
+                    raise RuntimeError(f"Duplicate match {pfd} : {weird}, existing match: {rename[pfd]})")
+                rename[pfd] = weird
+                del pfds[i]
+                del weird_files[j]
+                found = True
+    if len(pfds):
+        print(f"WARNING: unmatched pfds: {pfds}")
+
+    for pfd, weird in rename.items():
+        new_name = f"{pfd[:-3]}ps"
+        if verbose:
+            print(f"Moving {weird} to {new_name}")
+        #os.rename(weird, new_name)
 
     if verbose:
         print("DONE")
-
 
 if __name__ == "__main__":
     all_tasks = ["ps_rename", "quickclf", "quickclf_parse", "plot_ffa", "plot_singlepulse", "cleanup", "tar"]
@@ -44,3 +54,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     rename_ps(args.dirname)
+
+# OK so it turns out this is super annoying because the character limit is based on the dir prepfold was run in
+    
